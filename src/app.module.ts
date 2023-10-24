3003eada97ad6c0e64c5c9af05b8d7dc8e82dc8d
Elias Lourenço <elias.oliveira@novaescola.org.br>
2023-10-23 22:21:36 -0300

    ADD mongoose and basic config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, MongooseModule.forRoot('mongodb://localhost/user-admin'), ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/users/schemas/user.schema.ts
@@ -0,0 +1,21 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { HydratedDocument } from "mongoose";
+
+export type UserDocument = HydratedDocument<User>;
+
+
+@Schema()
+export class User {
+  @Prop()
+  username: string;
+
+  @Prop()
+  password: string;
+
+  @Prop({ default: 1 })
+  active: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User)
+
+
